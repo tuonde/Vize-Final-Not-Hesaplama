0b00358ed1daa4df17134b615ee9757c9a9f83ed
Tunahan Onursal Demiral <tunahandemiral1963@gmail.com>
2024-12-21 14:57:16 +0300

    Vize Final Not Hesaplama Ekranı
    C# Windows Forms uygulaması

---
+++ Vize - Final Not Hesaplama/VizeFinal_Hesaplama/Form1.Designer.cs
@@ -0,0 +1,321 @@
+﻿namespace VizeFinal_Hesaplama
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblVize = new System.Windows.Forms.Label();
+            this.lblFinal = new System.Windows.Forms.Label();
+            this.txtVizeNot = new System.Windows.Forms.TextBox();
+            this.txtFinalNot = new System.Windows.Forms.TextBox();
+            this.lblFinalOran = new System.Windows.Forms.Label();
+            this.lblVizeOran = new System.Windows.Forms.Label();
+            this.txtFinalOran = new System.Windows.Forms.TextBox();
+            this.txtVizeOran = new System.Windows.Forms.TextBox();
+            this.txtGecmeNotu = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnHesapla = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lblDurum = new System.Windows.Forms.Label();
+            this.lblOrt = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(797, 66);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Palatino Linotype", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Location = new System.Drawing.Point(179, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(432, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Vize - Final Ortalama Hesaplama";
+            // 
+            // lblVize
+            // 
+            this.lblVize.AutoSize = true;
+            this.lblVize.Location = new System.Drawing.Point(69, 94);
+            this.lblVize.Name = "lblVize";
+            this.lblVize.Size = new System.Drawing.Size(83, 19);
+            this.lblVize.TabIndex = 1;
+            this.lblVize.Text = "Vize Notu:";
+            // 
+            // lblFinal
+            // 
+            this.lblFinal.AutoSize = true;
+            this.lblFinal.Location = new System.Drawing.Point(65, 138);
+            this.lblFinal.Name = "lblFinal";
+            this.lblFinal.Size = new System.Drawing.Size(87, 19);
+            this.lblFinal.TabIndex = 2;
+            this.lblFinal.Text = "Final Notu:";
+            // 
+            // txtVizeNot
+            // 
+            this.txtVizeNot.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.txtVizeNot.Location = new System.Drawing.Point(158, 91);
+            this.txtVizeNot.Name = "txtVizeNot";
+            this.txtVizeNot.Size = new System.Drawing.Size(100, 27);
+            this.txtVizeNot.TabIndex = 3;
+            // 
+            // txtFinalNot
+            // 
+            this.txtFinalNot.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.txtFinalNot.Location = new System.Drawing.Point(158, 135);
+            this.txtFinalNot.Name = "txtFinalNot";
+            this.txtFinalNot.Size = new System.Drawing.Size(100, 27);
+            this.txtFinalNot.TabIndex = 4;
+            // 
+            // lblFinalOran
+            // 
+            this.lblFinalOran.AutoSize = true;
+            this.lblFinalOran.Location = new System.Drawing.Point(60, 224);
+            this.lblFinalOran.Name = "lblFinalOran";
+            this.lblFinalOran.Size = new System.Drawing.Size(92, 19);
+            this.lblFinalOran.TabIndex = 6;
+            this.lblFinalOran.Text = "Final Oranı:";
+            // 
+            // lblVizeOran
+            // 
+            this.lblVizeOran.AutoSize = true;
+            this.lblVizeOran.Location = new System.Drawing.Point(64, 181);
+            this.lblVizeOran.Name = "lblVizeOran";
+            this.lblVizeOran.Size = new System.Drawing.Size(88, 19);
+            this.lblVizeOran.TabIndex = 5;
+            this.lblVizeOran.Text = "Vize Oranı:";
+            // 
+            // txtFinalOran
+            // 
+            this.txtFinalOran.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.txtFinalOran.Location = new System.Drawing.Point(158, 221);
+            this.txtFinalOran.Name = "txtFinalOran";
+            this.txtFinalOran.Size = new System.Drawing.Size(100, 27);
+            this.txtFinalOran.TabIndex = 8;
+            // 
+            // txtVizeOran
+            // 
+            this.txtVizeOran.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.txtVizeOran.Location = new System.Drawing.Point(158, 178);
+            this.txtVizeOran.Name = "txtVizeOran";
+            this.txtVizeOran.Size = new System.Drawing.Size(100, 27);
+            this.txtVizeOran.TabIndex = 7;
+            // 
+            // txtGecmeNotu
+            // 
+            this.txtGecmeNotu.BackColor = System.Drawing.SystemColors.InactiveBorder;
+            this.txtGecmeNotu.Location = new System.Drawing.Point(158, 270);
+            this.txtGecmeNotu.Name = "txtGecmeNotu";
+            this.txtGecmeNotu.Size = new System.Drawing.Size(100, 27);
+            this.txtGecmeNotu.TabIndex = 10;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(50, 273);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(102, 19);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Geçme Notu:";
+            // 
+            // btnHesapla
+            // 
+            this.btnHesapla.Location = new System.Drawing.Point(54, 325);
+            this.btnHesapla.Name = "btnHesapla";
+            this.btnHesapla.Size = new System.Drawing.Size(204, 31);
+            this.btnHesapla.TabIndex = 11;
+            this.btnHesapla.Text = "Hesapla";
+            this.btnHesapla.UseVisualStyleBackColor = true;
+            this.btnHesapla.Click += new System.EventHandler(this.btnHesapla_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.groupBox1.Controls.Add(this.lblDurum);
+            this.groupBox1.Controls.Add(this.lblOrt);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.groupBox1.Location = new System.Drawing.Point(323, 107);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(236, 206);
+            this.groupBox1.TabIndex = 12;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Sonuç";
+            this.groupBox1.Visible = false;
+            // 
+            // lblDurum
+            // 
+            this.lblDurum.AutoSize = true;
+            this.lblDurum.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblDurum.Location = new System.Drawing.Point(112, 104);
+            this.lblDurum.Name = "lblDurum";
+            this.lblDurum.Size = new System.Drawing.Size(63, 19);
+            this.lblDurum.TabIndex = 3;
+            this.lblDurum.Text = "Durum";
+            // 
+            // lblOrt
+            // 
+            this.lblOrt.AutoSize = true;
+            this.lblOrt.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblOrt.Location = new System.Drawing.Point(112, 66);
+            this.lblOrt.Name = "lblOrt";
+            this.lblOrt.Size = new System.Drawing.Size(85, 19);
+            this.lblOrt.TabIndex = 2;
+            this.lblOrt.Text = "Ortalama";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(37, 104);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(69, 19);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Durum:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(15, 66);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(91, 19);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Ortalama:";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Location = new System.Drawing.Point(0, 374);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(797, 46);
+            this.panel2.TabIndex = 13;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.label5.Location = new System.Drawing.Point(12, 15);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(526, 19);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Doğru sonuç için tüm bilgileri eksiksiz ve doğru girdiğinizden emin olun. ";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(612, 155);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(142, 137);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 14;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.ClientSize = new System.Drawing.Size(796, 420);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnHesapla);
+            this.Controls.Add(this.txtGecmeNotu);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtFinalOran);
+            this.Controls.Add(this.txtVizeOran);
+            this.Controls.Add(this.lblFinalOran);
+            this.Controls.Add(this.lblVizeOran);
+            this.Controls.Add(this.txtFinalNot);
+            this.Controls.Add(this.txtVizeNot);
+            this.Controls.Add(this.lblFinal);
+            this.Controls.Add(this.lblVize);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblVize;
+        private System.Windows.Forms.Label lblFinal;
+        private System.Windows.Forms.TextBox txtVizeNot;
+        private System.Windows.Forms.TextBox txtFinalNot;
+        private System.Windows.Forms.Label lblFinalOran;
+        private System.Windows.Forms.Label lblVizeOran;
+        private System.Windows.Forms.TextBox txtFinalOran;
+        private System.Windows.Forms.TextBox txtVizeOran;
+        private System.Windows.Forms.TextBox txtGecmeNotu;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnHesapla;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label lblDurum;
+        private System.Windows.Forms.Label lblOrt;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Vize - Final Not Hesaplama/VizeFinal_Hesaplama/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VizeFinal_Hesaplama
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        float vize=0, final=0, ortalama=0;
+        int vizeOran=1, finalOran=1, gecmeNotu=0;
+        private void btnHesapla_Click(object sender, EventArgs e)
+        {
+            vize = float.Parse(txtVizeNot.Text);
+            final = float.Parse(txtFinalNot.Text);
+            vizeOran = int.Parse(txtVizeOran.Text);
+            finalOran = int.Parse(txtFinalOran.Text);
+            gecmeNotu = int.Parse(txtGecmeNotu.Text);
+
+            if (vizeOran + finalOran != 100)
+                MessageBox.Show("Vize Ortalaması ve Final Ortalaması toplamı 100 olmalıdır.","Hata!",MessageBoxButtons.OK,MessageBoxIcon.Warning);
+            else
+            {
+                ortalama = (vize * vizeOran / 100) + (final * finalOran / 100);
+                groupBox1.Visible = true;
+                lblOrt.Text = ortalama.ToString();
+                if (ortalama < gecmeNotu)
+                    lblDurum.Text = "Kaldı";
+                else
+                    lblDurum.Text = "Geçti";
+            }
+           
+        }
+    }
+}
